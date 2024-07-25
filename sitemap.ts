90a59b6ad52696e59f536bbff55f3d4b058fb217
Dimoiu Bogdan <bogdan.dimoiu@itperspectives.ro>
2024-07-25 17:11:46 +0300

    Update

--- sitemap.ts
+++ sitemap.ts
@@ -6,26 +6,26 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     {
       url: `${URL}/about`,
       lastModified: new Date().toISOString(),
-      changeFrequency: "never",
-      priority: 0.5,
+      changeFrequency: "weekly",
+      priority: 0.7,
     },
     {
       url: `${URL}/services`,
       lastModified: new Date().toISOString(),
-      changeFrequency: "never",
-      priority: 0.5,
+      changeFrequency: "weekly",
+      priority: 0.7,
     },
     {
       url: `${URL}/contact`,
       lastModified: new Date().toISOString(),
-      changeFrequency: "never",
-      priority: 0.5,
+      changeFrequency: "weekly",
+      priority: 0.7,
     },
     {
       url: `${URL}/`,
       lastModified: new Date().toISOString(),
-      changeFrequency: "never",
-      priority: 0.5,
+      changeFrequency: "weekly",
+      priority: 0.8,
     },
   ];
 }
